46859f224488c88af9ffe3f73431de312d4d4492
chikuwa-ah <chikuwah21@gmail.com>
2024-07-26 00:43:00 +0900

    stage

--- main.js
+++ main.js
@@ -11,6 +11,16 @@ let temp = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ], [
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0]
+    ], [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -20,9 +30,47 @@ let temp = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ], [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    ], [
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+    ], [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
 
-]
+];
+let lv_score = 0, score = 0, round = 1;
+let game_flag = 0;
+let level_up = [];
+
+//****************************************************************************
+//                          initialize
+//****************************************************************************
 
 function init() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -38,28 +86,54 @@ function init() {
     ctx.fillRect(20, 20, canvas.width - 45, 15);
     ctx.fillRect(80, 695, 120, 15);
 
-    ctx.fillStyle = '#ddd';
+    ctx.fillStyle = '#eee';
     ctx.font = 'bold 40px sans-serif';
     text = 'PRESS SPACE!!';
     textWidth = ctx.measureText(text).width;
     ctx.fillText(text, (canvas.width - textWidth) / 2, 450);
+
+    game_flag = 0;
+    lv_score = 0, score = 0, round = 1, level_up = [];
 }
 init();
 
+
+
+let level;
 let PDx, PDy, PDw;
+
+
 function start() {
-    let x = 50, y = 120;
-    for (let i = 0; i < temp[0].length; i++) {
-        for (let j = 0; j < temp[0][i].length; j++) {
-            blockAdd(x, y, temp[0][i][j]);
-            x += 75;
+    let x = 45, y = 120;
+    level = 0, lv_score = 0;
+    let block_length = 0;
+    let stage = Math.floor(Math.random() * temp.length)
+    for (let i = 0; i < temp[stage].length; i++) {
+        for (let j = 0; j < temp[stage][i].length; j++) {
+            blockAdd(x, y, temp[stage][i][j]);
+            x += 76;
+            if (temp[stage][i][j] == 1) {
+                block_length++;
+            }
         }
-        x = 50, y += 40;
+        x = 45, y += 40;
     }
 
     PDx = 80, PDy = 695, PDw = 120;
-
+    let able = block_length;
+    let ls = [];
     ballAdd();
+    for (let i = 0; i < 6; i++) {
+        able = block_length / 6;
+        block_length -= able;
+        ls.push(Math.floor(able));
+    }
+    let k = 0;
+    for (let i = 0; i < ls.length; i++) {
+        k += ls[5 - i];
+        level_up.push(k);
+    }
+    console.log(level_up, ls);
     main();
 }
 
@@ -115,6 +189,8 @@ function main() {
                     } else {
                         ball[i].dx *= -1;
                     }
+                    score++;
+                    lv_score++;
                     break;
                 }
             } else {
@@ -124,24 +200,30 @@ function main() {
 
         //当たり判定たち(ブロック以外)
         if (ball[i].y - 12 <= 35) {
+            ball[i].y = 35 + 12;
             ball[i].dy *= -1;
         }
         if (ball[i].x + 12 >= canvas.width - 35 || ball[i].x - 12 <= 35) {
             ball[i].dx *= -1;
+            if (ball[i].x + 12 >= canvas.width - 35) {
+                ball[i].x = canvas.width - 35 - 12;
+            } else {
+                ball[i].x = 35 + 12;
+            }
         }
 
-        if (ball[i].y + 12 - PDy > 3 && ball[i].x + 12 >= PDx && ball[i].x - 12 <= PDx + PDw) {
+        if (ball[i].y + 12 - PDy > 3 && ball[i].y + 12 - PDy < 15 && ball[i].x + 12 >= PDx && ball[i].x - 12 <= PDx + PDw) {
             ball[i].dy = Math.abs(ball[i].dy) * -1;
             let half = PDw / 2;
             if (ball[i].x < PDx + half) {
                 ball[i].dx = Math.abs(((half + 12) / 2) / (ball[i].x - (PDx - 12)) * ball[i].dy) * -1;
-                if (ball[i].dx < ball[i].dy * 1.5) {
-                    ball[i].dx = ball[i].dy * 1.5
+                if (ball[i].dx < ball[i].dy * 1.3) {
+                    ball[i].dx = ball[i].dy * 1.3
                 }
             } else {
                 ball[i].dx = Math.abs((ball[i].x - (PDx + half)) / ((half + 12) / 2) * ball[i].dy);
-                if (ball[i].dx > ball[i].dy * -1.5) {
-                    ball[i].dx = ball[i].dy * -1.5
+                if (ball[i].dx > ball[i].dy * -1.3) {
+                    ball[i].dx = ball[i].dy * -1.3
                 }
             }
         }
@@ -151,13 +233,42 @@ function main() {
         }
     }
 
+    if (lv_score > level_up[level]) {
+        for (let i = 0; i < ball.length; i++) {
+            if (ball[i].dy > 0) {
+                ball[i].dy += 0.8;
+            } else {
+                ball[i].dy -= 0.8;
+            }
+        }
+        level++;
+        console.log('level up');
+    }
 
-    if (block_state.length == 0 || ball.length == 0) {
+
+    ctx.font = 'bold 20px sans-serif';
+    ctx.fillStyle = '#fff';
+    ctx.fillText('SCORE : ' + score, 50, canvas.height - 5)
+    ctx.fillText('ROUND : ' + round, 790, canvas.height - 5);
+
+    if (block_state.length == 0) {
         window.cancelAnimationFrame(id);
-        ball = [];
+        setTimeout(round_clear, 1500);
+    }
+    if (ball.length == 0) {
+        window.cancelAnimationFrame(id);
+        block_state = [];
+        setTimeout(over, 1500);
     }
 }
 
+function round_clear() {
+    ball = [];
+    level_up = [];
+    lv_score = 0;
+    round++;
+    start();
+}
 
 function collision(L, R, T, B, x, y, radius) {
     if (L - radius > x || R + radius < x || T - radius > y || B + radius < y) {
@@ -188,16 +299,14 @@ function collision(L, R, T, B, x, y, radius) {
     }
 }
 
-
 document.addEventListener('keydown', keyDownHandler, false);
-
 function keyDownHandler(e) {
-    if (e.keyCode == 32) {
+    if (e.keyCode == 32 && game_flag == 0) {
         start();
+        game_flag = 2;
     }
-
-    if (e.keyCode == 13) {
-        ballAdd();
+    if (e.keyCode == 13 && game_flag == 1) {
+        init();
     }
 
     if ((e.keyCode == 37 || e.keyCode == 65) && PDx >= 35) {
@@ -215,10 +324,28 @@ function keyDownHandler(e) {
 }
 
 document.addEventListener('mousemove', mouseMove, false);
-
 function mouseMove(e) {
     PDx = e.offsetX - (PDw / 2) + 8
 }
+
+function over() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#FFF';
+    ctx.fillRect(20, 20, 15, canvas.height);
+    ctx.fillRect(canvas.width - 40, 20, 15, canvas.height);
+    ctx.fillRect(20, 20, canvas.width - 45, 15);
+    ctx.fillRect(PDx, PDy, PDw, 15);
+    ctx.font = 'bold 80px sans-serif';
+    let text = 'GAME OVER';
+    let textWidth = ctx.measureText(text).width;
+    ctx.fillText(text, (canvas.width - textWidth) / 2, 350);
+    ctx.font = 'bold 50px sans-serif';
+    text = 'SCORE : ' + score;
+    textWidth = ctx.measureText(text).width;
+    ctx.fillText(text, (canvas.width - textWidth) / 2, 450);
+    game_flag = 1;
+}
+
 
 //CLASSES
 
@@ -236,7 +363,7 @@ let block_state = [];
 function blockAdd(fcx, fcy, fcstate) {
     let b_x = fcx;
     let b_y = fcy;
-    let b_w = 65;
+    let b_w = 66;
     let b_h = 30;
     let b_state = fcstate;
 
@@ -255,10 +382,10 @@ class Ball {
 
 let ball = [];
 function ballAdd() {
-    let ball_x = 500;
+    let ball_x = 520;
     let ball_y = 700;
-    let ball_dx = -0;
-    let ball_dy = -7;
+    let ball_dx = 0;
+    let ball_dy = -2;
 
     let b = new Ball(ball_x, ball_y, ball_dx, ball_dy);
     ball.push(b);
